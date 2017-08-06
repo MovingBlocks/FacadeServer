fff7f0effdd4f2571de3ab6946747db59e32012f
Gianluca Nitti <gianluca.nitti96@gmail.com>
2017-08-06 19:36:17 +0200

    Add resources to obtain or modify server's MOTD and port

--- src/main/java/org/terasology/web/resources/ResourceManager.java
+++ src/main/java/org/terasology/web/resources/ResourceManager.java
@@ -23,6 +23,8 @@ import org.terasology.engine.modes.StateIngame;
 import org.terasology.entitySystem.systems.ComponentSystem;
 import org.terasology.registry.InjectionHelper;
 import org.terasology.web.io.ActionResult;
+import org.terasology.web.resources.config.ServerMotdResource;
+import org.terasology.web.resources.config.ServerPortResource;
 import org.terasology.web.resources.games.GamesResource;
 import org.terasology.web.resources.modules.AvailableModulesResource;
 
@@ -52,6 +54,8 @@ public final class ResourceManager {
         registerAndPutResource(context, new EngineStateResource());
         registerAndPutResource(context, new GamesResource());
         registerAndPutResource(context, new AvailableModulesResource());
+        registerAndPutResource(context, new ServerMotdResource());
+        registerAndPutResource(context, new ServerPortResource());
         if (gameState instanceof StateIngame) {
             registerAndPutResource(context, new ConsoleResource());
             registerAndPutResource(context, new OnlinePlayersResource());
