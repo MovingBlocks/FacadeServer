64da6756519966f80cc08019de217edd3861f401
Inei1 <abnerd951@gmail.com>
2018-07-05 19:14:10 -0700

    add y-coordinate brightness and improve image size code

--- src/main/java/org/terasology/web/resources/worldMap/WorldMapResource.java
+++ src/main/java/org/terasology/web/resources/worldMap/WorldMapResource.java
@@ -18,6 +18,7 @@ package org.terasology.web.resources.worldMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
+import org.terasology.math.TeraMath;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
 import org.terasology.utilities.Assets;
@@ -41,7 +42,6 @@ import java.util.Base64;
 import java.util.List;
 
 import static org.terasology.web.resources.base.ResourceMethodFactory.createParameterlessMethod;
-import static org.terasology.web.resources.base.ResourceMethodFactory.createVoidParameterlessMethod;
 
 public class WorldMapResource extends AbstractSimpleResource {
 
@@ -49,13 +49,6 @@ public class WorldMapResource extends AbstractSimpleResource {
 
     @In
     private WorldProvider worldProvider;
-
-    // This temporary GET method is needed to keep the frontend from displaying errors
-    /*@Override
-    protected ResourceMethod<Void, String> getGetMethod(ResourcePath path) throws ResourceAccessException {
-        return createVoidParameterlessMethod(path, ClientSecurityRequirements.PUBLIC, Void.class,
-                (data, client) -> getWorldMapBase64ImageString(new Vector3i(0, 0, 0), 0, 0, true));
-    }*/
 
     // TODO: reimplement this as a GET request with parameters
     @Override
@@ -65,30 +58,38 @@ public class WorldMapResource extends AbstractSimpleResource {
     }
 
     private String getWorldMapBase64ImageString(Vector3i center, int mapBlockWidth, int mapBlockLength, boolean isSurface) {
-        final int colorSizeMultiplier = 30;
-        int y = 40;
-        List<Color> colors = new ArrayList<>();
+        final int colorSizeMultiplier = mapBlockWidth * mapBlockLength <= 125 * 125 ? 60 : 30;
+        int blockY = 40;
+        List<List<Color>> colors = new ArrayList<>(mapBlockWidth);
+        for (int i = 0; i < mapBlockWidth; ++i) {
+            colors.add(i, new ArrayList<>(mapBlockLength));
+        }
+
         for (int x = (int) Math.floor((double) center.getX() - mapBlockWidth / 2); x < (int) Math.ceil((double) mapBlockWidth / 2 + center.getX()); ++x) {
             for (int z = (int) Math.floor((double) center.getZ() - mapBlockLength / 2); z < (int) Math.ceil((double) mapBlockLength / 2 + center.getZ()); ++z) {
-                y = isSurface ? getSurfaceY(x, y, z) : center.getY();
-                Block block = worldProvider.getBlock(x, y, z);
+                blockY = isSurface ? getSurfaceY(x, blockY, z) : center.getY();
+                Block block = worldProvider.getBlock(x, blockY, z);
                 ResourceUrn blockUrn = block.getURI().getBlockFamilyDefinitionUrn();
                 if (Assets.get(blockUrn, BlockTile.class).isPresent()) {
                     BufferedImage blockImage = Assets.get(blockUrn, BlockTile.class).get().getImage();
-                    colors.add(new Color(getColorOfTexture(blockImage)));
+                    colors.get(x - (center.getX() - mapBlockWidth / 2)).add(new Color(getColorOfTexture(blockImage, blockY)));
                 } else {
-                    logger.error("cannot find texture of block " + blockUrn.toString());
-                    colors.add(Color.BLACK);
+                    logger.warn("cannot find texture of block " + blockUrn.toString());
+                    colors.get(x - (center.getX() - mapBlockWidth / 2)).add(Color.BLACK);
                 }
             }
         }
-        int colorSize = (int) (1 / Math.floor(Math.log(colors.size())) * colorSizeMultiplier);
+
+        int colorSize = (int) (1 / Math.floor(Math.log(mapBlockWidth * mapBlockLength)) * colorSizeMultiplier);
         BufferedImage mapImage = new BufferedImage(mapBlockWidth * colorSize, mapBlockLength * colorSize, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics = mapImage.createGraphics();
-        for (int i = 0; i < colors.size(); ++i) {
-            graphics.setColor(colors.get(i));
-            graphics.fillRect(colorSize * (int) Math.ceil(i / mapBlockLength), colorSize * (i % mapBlockWidth), colorSize, colorSize);
+        for (int x = 0; x < mapBlockWidth; ++x) {
+            for (int y = 0; y < mapBlockLength; ++y) {
+                graphics.setColor(colors.get(x).get(y));
+                graphics.fillRect(colorSize * x, colorSize * y, colorSize, colorSize);
+            }
         }
+
         graphics.dispose();
         return convertImageToBase64String(mapImage);
     }
@@ -104,8 +105,10 @@ public class WorldMapResource extends AbstractSimpleResource {
         return null;
     }
 
-    // TODO use y coordinate to increase/decrease brightness of texture (lower is darker)
-    private int getColorOfTexture(BufferedImage bufferedImage) {
+    private int getColorOfTexture(BufferedImage bufferedImage, int blockY) {
+        double brightnessMin = 0.85;
+        double blockYCoordinateBrightnessChange = 0.0015;
+        double brightnessMultiplier = TeraMath.clamp(brightnessMin + blockY * blockYCoordinateBrightnessChange, brightnessMin, 1);
         int r = 0;
         int g = 0;
         int b = 0;
@@ -119,8 +122,11 @@ public class WorldMapResource extends AbstractSimpleResource {
         }
         int imageSize = bufferedImage.getWidth() * bufferedImage.getHeight();
         r /= imageSize;
+        r *= brightnessMultiplier;
         g /= imageSize;
+        g *= brightnessMultiplier;
         b /= imageSize;
+        b *= brightnessMultiplier;
         // get rid of the alpha because it is always 255
         return new Color(r, g, b).getRGB() & 0x00FFFFFF;
     }
